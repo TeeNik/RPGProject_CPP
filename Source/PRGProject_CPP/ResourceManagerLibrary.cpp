7f0c6f34ed5c5c045387129b37dfa5c62afe85f2
TeeNik <teenik3315@gmail.com>
2018-03-24 21:20:59 +0300

    projectile script

--- Source/PRGProject_CPP/ResourceManagerLibrary.cpp
+++ Source/PRGProject_CPP/ResourceManagerLibrary.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ResourceManagerLibrary.h"
+#include "Engine/Engine.h"
 
 UResourceManagerLibrary::UResourceManagerLibrary(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
 
