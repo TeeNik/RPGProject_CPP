0c55b5f7bb61ed8149e65abf7f96353fb8a57971
TeeNik <teenik3315@gmail.com>
2018-03-28 01:31:41 +0300

    health hud

--- Source/PRGProject_CPP/PlayerHealth.h
+++ Source/PRGProject_CPP/PlayerHealth.h
@@ -6,7 +6,6 @@
 #include "Components/ActorComponent.h"
 #include "PlayerHealth.generated.h"
 
-
 UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class PRGPROJECT_CPP_API UPlayerHealth : public UActorComponent
 {
@@ -18,15 +17,10 @@ public:
 	void TakeDamage(float value);
 	bool TakeMana(float value);
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
-	float MaxHealth;
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
-	float MaxMana;
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
-	float Health;
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
-	float Mana;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere) float MaxHealth;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere) float MaxMana;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere) float Health;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere) float Mana;
 
 protected:
 	virtual void BeginPlay() override;
